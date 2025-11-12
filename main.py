ca1467356bc70dad5f57cd0b59c5f695d931b863
yapyx <yapyx317@gmail.com>
2025-11-12 17:57:08 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,12 +25,20 @@ from langchain_tavily import TavilySearch
 #    print(f"Searching for {query}")
 #    return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 
 llm = ChatOpenAI(model="gpt-5")
 # tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm,tools=tools)
+agent = create_agent(model=llm,tools=tools,response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
